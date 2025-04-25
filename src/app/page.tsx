7ce8eb0f5746635c6b4650b46d2016faa0a82abb
Tuong Vo <Tuong@mixto.net>
2025-04-25 01:17:56 -0400

    text motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-import { motion } from "framer-motion";
+import * as motion from "motion/react-client"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex gap-[4px] row-start-2 items-center sm:items-start">
+      <main className="flex items-baseline gap-[4px] row-start-2">
         <motion.div
           className="name"
-          initial={{ opacity: 0, y: -20 }}
+          initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
-          Mạnh Tường 1
+          Mạnh Tường
         </motion.div>
         <motion.div
           className="va text-center sm:text-left"
-          initial={{ opacity: 0, y: -20 }}
+          initial={{ opacity: 0, y: 0 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
@@ -28,7 +28,14 @@ export default function Home() {
         >
           Thiên Kim
         </motion.div>
+
       </main>
     </div>
   );
+}
+const box = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#9911ff",
+    borderRadius: 5,
 }

03cbf38919a8c787bdf8a0b7289a6012974f0fa3
Tuong Vo <56697560+tuongmanhvo@users.noreply.github.com>
2025-05-08 10:05:12 -0400

    Update AlbumIndex.tsx

--- src/components/AlbumIndex.tsx
+++ src/components/AlbumIndex.tsx
@@ -10,11 +10,11 @@ const AlbumIndex = () => {
             <div className='flex items-center justify-center flex-col w-full gap-20 lg:flex-row lg:gap-10'>
 
                 <div className="flex flex-col gap-3 font-bold">
-                    <span className="text-4xl text-center md:text-left lg:text-left" style={{ fontFamily: 'Times New Roman' }}>KHOẢNG KHẮC</span>
+                    <span className="text-4xl text-center md:text-left lg:text-left" style={{ fontFamily: 'Times New Roman' }}>KHOẢNH KHẮC</span>
                     <span className="text-4xl text-center md:text-left lg:text-left" style={{ fontFamily: 'Times New Roman' }}>AN YÊN</span>
                 </div>
                 <div className="flex flex-col justify-center gap-4 mx-4 lg:items-end">
-                    <span>Bộ ảnh này ghi lại những khoảng khắc dịu dàng,</span>
+                    <span>Bộ ảnh này ghi lại những khoảnh khắc dịu dàng,</span>
                     <span>nơi ánh mắt trao nhau nhiều hơn vạn lời nói.</span>
                     <span>Một chương mở đầu ngọt ngào cho hành trình dài mang tên <span className='font-bold'>&quot;Chúng Mình&quot;</span>.</span>
                 </div>
